d2364bba76a19dce606187bbf2923e5e42b2061a
DipankarHalder <dipankar-halder@outlook.com>
2021-05-10 02:00:44 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
+let itemCountry = document.getElementById('allCountries');
+
 async function fetchCountry(pageNo) {
+    let listCountry;
     const apiPath = `https://jsonmock.hackerrank.com/api/countries?page=${pageNo}`;
     try {
         const response = await fetch(apiPath);
         const countries = await response.json();
-        console.log(countries);
+        countries.data.map(item => 
+            listCountry += 
+            `<li>
+                <h3>${item.name}</h3>
+                <p>${item.nativeName}</p>
+                <p>${item.capital}</p>
+                <p>${item.region}</p>
+            </li>`
+        );
+        itemCountry.innerHTML = listCountry;
     } catch (error) {
         console.log('Fetch error: ', error);
     }
 }
 
-fetchCountry(1);
+fetchCountry(11);
